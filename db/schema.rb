b56cb706c7ec151628dd6448b0dff757e86cc03f
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-04-22 02:07:30 +0800

    Migrate add trackable to Devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_21_150712) do
+ActiveRecord::Schema.define(version: 2019_04_21_180413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,11 @@ ActiveRecord::Schema.define(version: 2019_04_21_150712) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["peran_id"], name: "index_users_on_peran_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
