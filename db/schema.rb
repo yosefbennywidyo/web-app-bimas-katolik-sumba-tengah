1468faba0b45033c8e8db548ea61f7686945f340
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-04-21 22:38:03 +0800

    Migrate changing to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_21_124308) do
+ActiveRecord::Schema.define(version: 2019_04_21_125353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,14 @@ ActiveRecord::Schema.define(version: 2019_04_21_124308) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "peran_id"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["peran_id"], name: "index_users_on_peran_id"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "data_keagamaan_katolik", "users"
