415799be6cd1d913b24f3996cb37cb2604439ed1
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-04-21 20:34:33 +0800

    Set Beranda with index action as root

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :data_keagamaan_katolik
   resources :peran
   resources :users
+  root 'beranda#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
