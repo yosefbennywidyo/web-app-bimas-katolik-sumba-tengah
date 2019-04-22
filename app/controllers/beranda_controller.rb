915514cea2cfb52c60916f3ff8b3ad2a717a1373
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-04-23 02:12:00 +0800

    Add DataPendidikanAgamaKatolik to Beranda

--- app/controllers/beranda_controller.rb
+++ app/controllers/beranda_controller.rb
@@ -1,12 +1,16 @@
 class BerandaController < ApplicationController
   def index
   	@data_keagamaan_katolik = DataKeagamaanKatolik.all
+  	@data_pendidikan_agama_katolik = DataPendidikanAgamaKatolik.all
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
 
     # Never trust parameters from the scary internet, only allow the white list through.
+    def data_pendidikan_agama_katolik
+      params.require(:data_pendidikan_agama_katolik).permit(:judul, :keterangan, :tautan, :user_id)
+    end
     def data_keagamaan_katolik_params
       params.require(:data_keagamaan_katolik).permit(:judul, :keterangan, :tautan, :user_id)
     end
